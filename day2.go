59ad7982b673a7646a404503bf17527a0f14dcc0
ryandemarigny <ryandemarigny@gmail.com>
2025-01-09 21:28:21 -0800

    day 2 done

--- day2.go
+++ day2.go
@@ -31,6 +31,35 @@ func parseReportsFromPuzzleInput(puzzleInputName string) ([][]int, error) {
 	return reports, nil
 }
 
+func isReportSafe(report []int) bool {
+	increasing := true
+	decreasing := true
+	largestDifference := 0.0
+
+	for i := range report {
+		if i + 1 >= len(report) {
+			break
+		}
+
+		if report[i + 1] > report[i] {
+			decreasing = false
+		} else if report[i + 1] < report[i] {
+			increasing = false
+		} else {
+			decreasing = false
+			increasing = false
+		}
+
+		difference := math.Abs(float64(report[i + 1]) - float64(report[i]))
+		if difference > largestDifference {
+			largestDifference = difference
+		}
+	}
+
+	return (increasing || decreasing) && largestDifference <= 3.0
+}
+
+
 func Day2Part1(puzzleInputName string) (string, error) {
 	reports, err := parseReportsFromPuzzleInput(puzzleInputName)
 	if err != nil {
@@ -40,38 +69,37 @@ func Day2Part1(puzzleInputName string) (string, error) {
 	numSafe := len(reports)
 
 	for _, report := range reports {
-		increasing := true
-		decreasing := true
-		largestDifference := 0.0
-
-		for i := range report {
-			if i + 1 >= len(report) {
-				break
-			}
-
-			if report[i + 1] > report[i] {
-				decreasing = false
-			} else if report[i + 1] < report[i] {
-				increasing = false
-			} else {
-				decreasing = false
-				increasing = false
-			}
-
-			difference := math.Abs(float64(report[i + 1]) - float64(report[i]))
-			if difference > largestDifference {
-				largestDifference = difference
-			}
-		}
-
-		if (!increasing && !decreasing) || largestDifference > 3.0 {
+		if !isReportSafe(report) {
 			numSafe--
 		}
 	}
 	return strconv.Itoa(numSafe), nil
 }
 
-
 func Day2Part2(puzzleInputName string) (string, error) {
-	return "", nil
+	reports, err := parseReportsFromPuzzleInput(puzzleInputName)
+	if err != nil {
+		return stringErrorValue, err
+	}
+
+	numSafe := len(reports)
+
+	for _, report := range reports {
+		if isReportSafe(report) {
+			continue
+		}
+
+		safe := false
+		for i := range report {
+			alteredReport := CopySliceWithIndexExcluded(report, i)
+			if isReportSafe(alteredReport) {
+				safe = true
+				break
+			}
+		}
+		if !safe {
+			numSafe--
+		}
+	}
+	return strconv.Itoa(numSafe), nil
 }
--- day2_test.go
+++ day2_test.go
@@ -5,6 +5,7 @@ import "testing"
 const (
 	day2PuzzleExampleInputName = "day2example"
 	day2Part1ExampleCorrectAnswer = "2"
+	day2Part2ExampleCorrectAnswer = "4"
 )
 
 func TestDay2Part1(t *testing.T) {
@@ -15,5 +16,17 @@ func TestDay2Part1(t *testing.T) {
 
 	if answer != day2Part1ExampleCorrectAnswer {
 		t.Errorf("answer: %s != expected: %s", answer, day2Part1ExampleCorrectAnswer)
+	}
+}
+
+
+func TestDay2Part2(t *testing.T) {
+	answer, err := Day2Part2(day2PuzzleExampleInputName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if answer != day2Part2ExampleCorrectAnswer {
+		t.Errorf("answer: %s != expected: %s", answer, day2Part2ExampleCorrectAnswer)
 	}
 }
