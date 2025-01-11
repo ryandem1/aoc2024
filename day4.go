4a44799f95ea81c65ab604c6cba8f26f268877d6
ryandemarigny <ryandemarigny@gmail.com>
2025-01-10 20:58:43 -0800

    initialized day 4

---
+++ day4.go
@@ -0,0 +1,10 @@
+package main
+
+func Day4Part1(puzzleInputName string) (string, error) {
+	return puzzleInputName, nil
+}
+
+
+func Day4Part2(puzzleInputName string) (string, error) {
+	return puzzleInputName, nil
+}
